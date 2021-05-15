18b22fdc36fc340a85275b7e34dbd6adf096f786
lathikaMohan <73482831+lathikaMohan@users.noreply.github.com>
2021-05-15 20:53:54 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,40 @@
+  
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var snow;
+var snowFlake=[];
+
+function setup() {
+  createCanvas(800,400); 
+  engine=Engine.create();
+   world=engine.world;
+//  createSprite(400, 200, 50, 50);
+
+}
+
+function preload() {
+
+  snow = loadImage("Snow BG.jpg");
+}
+
+function draw() {
+  background(snow);  
+
+  if(frameCount % 5===0){
+    snowFlake.push(new Snow(random (10, 790),30, 50) )
+    
+  }
+
+  for(var i = 0;i<snowFlake.length; i =i+1){
+    snowFlake[i].display();
+
+  }
+  
+ // drawSprites();
+}
+
+
+
+
